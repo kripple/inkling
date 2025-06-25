2d958b2f283c1bdd55d9eb33833207022037c6b6
Kelly Ripple <kelly.m.ripple@gmail.com>
2025-06-25 17:37:15 -0400

    add server code

---
+++ src/server/start.ts
@@ -0,0 +1,36 @@
+import { createServer } from 'node:http';
+import { Server } from 'socket.io';
+import { Rectangle } from './store';
+
+// TODO: enforce a max number of rectangles
+// TODO: add reset/clear button to remove all rectangles
+
+const port = 3000 as const;
+const server = createServer();
+const io = new Server<ClientEvents, ServerEvents>(server, {
+  cors: {
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST'],
+  },
+});
+
+io.on('connection', (socket) => {
+  socket.emit('ready');
+
+  // Send current state to newly connected client
+  socket.emit('rectangle:init', Rectangle.list());
+
+  socket.on('rectangle:add', (data) => {
+    const rectangle = Rectangle.create({ ...data, socketId: socket.id });
+    io.emit('rectangle:add', rectangle); // broadcast to all
+  });
+
+  socket.on('rectangle:move', (rectangle) => {
+    Rectangle.update(rectangle);
+    socket.broadcast.emit('rectangle:move', rectangle); // broadcast to others only
+  });
+});
+
+server.listen(port, () => {
+  console.log(`server listening at http://localhost:${port}`);
+});
---
+++ src/server/store.ts
@@ -0,0 +1,21 @@
+import { observable } from 'mobx';
+
+export const Rectangle = observable({
+  rectangles: {} as RectangleEntries,
+  currentId: 0,
+
+  list(): RectangleEntries {
+    return this.rectangles;
+  },
+
+  create(rectangle: Omit<RectangleEntry, 'id'>) {
+    const id = String(this.currentId++);
+    const draft = { ...rectangle, id };
+    this.rectangles[id] = draft;
+    return draft;
+  },
+
+  update(rectangle: RectangleEntry) {
+    this.rectangles[rectangle.id] = rectangle;
+  },
+});
