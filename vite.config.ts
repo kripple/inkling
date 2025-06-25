2b8cee7c6227c6e6f2042d9463b8efc56f13a585
Kelly Ripple <kelly.m.ripple@gmail.com>
2025-06-25 12:20:46 -0400

    add tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react-swc';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/inkling/',
   build: { outDir: 'docs' },
   clearScreen: false,
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   server: {
     open: true,
   },
