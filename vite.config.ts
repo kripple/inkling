0eb71f63708f05af2875589b65810d0bbb002a01
Kelly Ripple <kelly.m.ripple@gmail.com>
2025-06-25 10:32:49 -0400

    deploy react app to github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/inkling/',
+  build: { outDir: 'docs' },
+  clearScreen: false,
   plugins: [react()],
-})
+  server: {
+    open: true,
+  },
+});
